e1ab83a764e81097a2bb8fc852276da38b181b11
unknown <libinbiji43@gmail.com>
2024-09-23 21:39:49 +0530

    update: using map in js

--- ARRAY/dup.js
+++ ARRAY/dup.js
@@ -1,13 +1,27 @@
-const arr = [2,3,5,2,3,6,4,2]
-let hashMap = {}
-const duplicate =[]
-function dup (arr){
-    for(let i =0;i<arr.length;i++){
-        hashMap[arr[i]] =  hashMap[arr[i]] ? hashMap[arr[i]]+1 :1
-        console.log(hashMap)
+const arr = [2,3,5,2,3,6,4,2,4]
+// let hashMap = {}
+// function dup (arr){
+//     for(let i =0;i<arr.length;i++){
+//         hashMap[arr[i]] =  hashMap[arr[i]] ? hashMap[arr[i]]+1 :1
+//         console.log(hashMap)
        
-    }
+//     }
 
+// }
+
+// console.log(dup(arr));
+
+function findDuplicates(arr) {
+    const map = new Map()
+    for(let i=0; i< arr.length; i++) {
+        if(map.has(arr[i])){
+            map.set(arr[i], map.get(arr[i])+1)
+        } else {
+            map.set(arr[i],1)
+        }
+    }
+    console.log(map);
+    
 }
 
-console.log(dup(arr));
+const res = findDuplicates(arr)
